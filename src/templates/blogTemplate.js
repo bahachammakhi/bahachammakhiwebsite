8ab08c103109b79469927a10e3e7aac815b661c3
Baha chammakhi <bahachammakhi25@gmail.com>
2021-04-21 03:41:21 +0100

    added: SEO

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
+import { SEO } from "../components/SEO";
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
@@ -8,6 +9,19 @@ export default function Template({
   const { frontmatter, html } = markdownRemark;
   return (
     <Layout>
+      <SEO
+        type="NewsArticle"
+        title={frontmatter.title}
+        articleBody={frontmatter.description}
+        datePublished={frontmatter.normalDate}
+        dateModified={
+          frontmatter.edited ? frontmatter.edited : frontmatter.date
+        }
+        cover={frontmatter.thumbnail}
+        location={frontmatter.slug}
+        description={frontmatter.description}
+        readTime={frontmatter.timeToRead}
+      />
       <article className="container m-auto pt-20">
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
